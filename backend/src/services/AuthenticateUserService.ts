97370d5e2b8c809a2128256cb1288e6984b50458
Henrique Ritter <henrique-ritter@hotmail.com>
2021-10-21 18:56:20 -0300

    Created routes.ts/created execute method on servic

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,29 @@
+/** -Fluxo-
+ * Recer code(string)
+ * Recuperar access_token do github para acessar as infos do user
+ * Verificar se usuário existe no DB, caso sim gere token, caso nao cria no DB e gera um token
+ * Retornar o token com as infos do user logado.
+ */
+import axios from 'axios';
 
 class AuthenticateUserService {
-  async execute(code: string) { }
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    //url, body(nulo), params
+    const response = await axios.post(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code
+      },
+      headers: {
+        "Accept": "application/json"
+      }
+    });
+
+    return response.data;
+  }
 }
 
 export { AuthenticateUserService }
