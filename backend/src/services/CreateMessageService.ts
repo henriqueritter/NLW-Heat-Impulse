2c85aca3db4d16cb958e866239aa64df4a905566
Henrique Ritter <henrique-ritter@hotmail.com>
2021-10-26 20:12:09 -0300

    message create on CreateMsgService with user relat

--- backend/src/services/CreateMessageService.ts
+++ backend/src/services/CreateMessageService.ts
@@ -1,5 +1,18 @@
+import PrismaClient from '../prisma'
 class CreateMessageService {
-  async execute(text: string, user_id: string) { }
+  async execute(text: string, user_id: string) {
+    const message = await PrismaClient.message.create({
+      data: {
+        text,
+        user_id
+      },
+      include: {  //retorna as informacoes do user pela relacao com message
+        user: true
+      }
+    });
+
+    return message;
+  }
 }
 
 export { CreateMessageService }
