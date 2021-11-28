5d838643d4ef1b360c7237b0bb41ff05863c0526
Henrique Ritter <henrique-ritter@hotmail.com>
2021-11-28 14:17:52 -0300

    Created ProfileUserController.ts to list user

---
+++ backend/src/controllers/ProfileUserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { ProfileUserService } from '../services/ProfileUserService';
+
+class ProfileUserController {
+  async handle(request: Request, response: Response) {
+    const { user_id } = request;
+
+    const profileUserService = new ProfileUserService();
+
+    const result = profileUserService.execute(user_id);
+
+    return response.json(result);
+  }
+}
+export { ProfileUserController }
