02d4074046b240b54f38c537ff3a65e5b31c9c7f
Henrique Ritter <henrique-ritter@hotmail.com>
2021-11-28 12:31:32 -0300

    Created Get3LastMessageController

--- backend/src/services/GetLast3MessagesService.ts
+++ backend/src/services/GetLast3MessagesService.ts
@@ -1,7 +1,23 @@
+import prismaClient from '../prisma'
 import PrismaClient from '../prisma'
 
 class GetLast3MessagesService {
-  async execute() { }
+  async execute() {
+    //retorna apenas 3 linhas
+    //SELECT * FROM MESSAGES LIMIT 3 ORDER BY CREATED_AT DESC;
+    const messages = await prismaClient.message.findFirst({
+      take: 3,
+      orderBy: {
+        created_at: "desc"
+      },
+      //retorna os usuarios tbm
+      include: {
+        user: true
+      }
+    })
+
+    return messages;
+  }
 }
 
 
